5ac0a88e7a27f000ae21c6505f95ec624cf876dc
alessandriLuca <alessandri.luca1991@gmail.com>
2021-10-20 13:52:26 +0200

    Delete PythonPackages/Python3.8.6/dockerFolder directory

--- PythonPackages/Python3.8.6/dockerFolder/toBeInstalled/listForDockerfile.sh
+++
@@ -1,5 +0,0 @@
-pip3 install --no-dependencies /tmp/packages/six-1.16.0-py2.py3-none-any.whl
-pip3 install --no-dependencies /tmp/packages/pytz-2021.3-py2.py3-none-any.whl
-pip3 install --no-dependencies /tmp/packages/python_dateutil-2.8.2-py2.py3-none-any.whl
-pip3 install --no-dependencies /tmp/packages/numpy-1.21.2-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl
-pip3 install --no-dependencies /tmp/packages/pandas-1.3.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
