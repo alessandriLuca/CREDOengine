eae19206152722e51d0f950dbe347f6bae3aa767
alessandriLuca <alessandri.luca1991@gmail.com>
2021-12-05 12:26:13 +0100

    Delete PythonPackages directory

--- PythonPackages/Python2.7.18/configurationFile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-#type here all libraries that needs to be installed
-pip2 -v --log /scratch/pip.log install numpy
-pip2 -v --log /scratch/pip.log install pandas
-
--- PythonPackages/Python3.8.6/configurationFile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-#type here all libraries that needs to be installed
-pip3 -v --log /scratch/pip.log install pandas
-
