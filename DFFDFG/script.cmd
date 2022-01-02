9cdf46237fdc69e375b9703d09efff3f7e24a521
lucastormreig <alessandri.luca1991@gmail.com>
2022-01-02 15:12:22 +0100

    something

--- DFFDFG/script.cmd
+++ DFFDFG/script.cmd
@@ -3,4 +3,4 @@ set /p Build=<configurationFile.txt
 mkdir %Build%
 copy configurationFile.txt %Build%
 del %Build%\id.txt
-docker run -itv %Build%:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id.txt  -p  8888:8888 repbioinfo/dockerfilegeneratorv3
+docker run -itv %Build%:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id.txt  -p  8888:8888 -p 3000:3000 repbioinfo/dockerfilegeneratorv3
--- DFFDFG/script.sh
+++ DFFDFG/script.sh
@@ -3,4 +3,4 @@ tt=$(head configurationFile.txt)
 mkdir $tt
 cp ./configurationFile.txt $tt
 rm $tt\id.txt
-docker run -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt\id.txt --privileged=true -p  8888:8888 repbioinfo/dockerfilegeneratorv3
+docker run -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt\id.txt --privileged=true -p  8888:8888 -p  3000:3000 repbioinfo/dockerfilegeneratorv3
