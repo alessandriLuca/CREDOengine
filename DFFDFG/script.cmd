a47182b17dc12fb2a0a3fc2a390a3c81c2e80a3e
alessandriLuca <alessandri.luca1991@gmail.com>
2022-02-02 09:23:41 +0100

    Delete DFFDFG directory

--- DFFDFG/script.cmd
+++
@@ -1,6 +0,0 @@
-docker build . -t  repbioinfo/dockerfilegeneratorv3
-set /p Build=<configurationFile.txt
-mkdir %Build%
-copy configurationFile.txt %Build%
-del %Build%\id.txt
-docker run -itv %Build%:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --privileged=true --cidfile  %Build%\id.txt  -p  8888:8888 -p 3000:3000 repbioinfo/dockerfilegeneratorv3
--- DFFDFG/script.sh
+++
@@ -1,6 +0,0 @@
-docker build . -t  repbioinfo/dockerfilegeneratorv3
-tt=$(head configurationFile.txt)
-mkdir $tt
-cp ./configurationFile.txt $tt
-rm $tt\id.txt
-docker run -itv $tt:/sharedFolder -v /var/run/docker.sock:/var/run/docker.sock --cidfile  $tt\id.txt --privileged=true -p  8888:8888 -p  3000:3000 repbioinfo/dockerfilegeneratorv3
