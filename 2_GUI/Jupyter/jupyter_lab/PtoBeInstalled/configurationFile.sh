bab6f9e72a604e77f4e8f4ae01fa62bff687a8c0
alessandriLuca <alessandri.luca1991@gmail.com>
2022-02-18 10:55:34 +0100

    Update configurationFile.sh

--- 2_GUI/Jupyter/jupyter_lab/PtoBeInstalled/configurationFile.sh
+++ 2_GUI/Jupyter/jupyter_lab/PtoBeInstalled/configurationFile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 pip3 -v --log /scratch/pip.log install jupyter
 pip3 -v --log /scratch/pip.log install jupyterlab
-
+pip3 -v --log /scratch/pip.log install lckr-jupyterlab-variableinspector
