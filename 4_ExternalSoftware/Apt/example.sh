bd5d34b8194a72629ec688f6e3d43c4027e07bb7
lucastormreig <alessandri.luca1991@gmail.com>
2023-06-24 13:11:10 -0400

    yo

--- 4_ExternalSoftware/Apt/example.sh
+++ 4_ExternalSoftware/Apt/example.sh
@@ -1,1 +1,2 @@
-./runMe.sh /home/lucastormreig/Desktop/CREDOOOOOOOOOOOOOOOOoo/CREDOengine/4_ExternalSoftware/jennifer/  /home/lucastormreig/dockerRes/
+./runMe.sh /home/lucastormreig/Desktop/CREDOOOOOOOOOOOOOOOOoo/CREDOengine/4_ExternalSoftware/Apt/ff/ tempdockername finalname /home/lucastormreig/dockerRes/
+#./runMe.sh /home/lucastormreig/dockerRes/merged/ tempdockername juplab /home/lucastormreig/dockerRes/ /home/lucastormreig/configurationFile.txt
--- 4_ExternalSoftware/Apt/runMe.sh
+++ 4_ExternalSoftware/Apt/runMe.sh
@@ -1,20 +1,54 @@
 #!/bin/bash 
+#1)merged File path
+#2) docker temp
+#3) Final Docker name
+#4) Final path
+#5) path to hostPath txt file.
 retry() {
     until "$@"
     do
             echo "Attempt failed! Trying again"
     done
 }
-if [ $# -ne 2 ]
+if [ $# -ne 4 ] &&  [ $# -ne 5 ]
   then
     echo "You need to provide :"
-        echo " The folderName of the mergedDockers  "
-                echo " The result Folder"
+        echo " The absolute path to the mergedDockers  "
+    echo " a temporary docker name  "
+    echo " DockerName to build the new docker container "
+        echo " A temporary folder path "
+    echo "Path to a file containing the hostPath, for Indocker user only "
+   echo $#
     exit
 fi
-j="$( basename "$1" )"
 
-pathSharedfoldDocks=$2/${j}_wExternalFile
+if [ $# -eq 4 ]
+then
+echo "hope you are on IOS or Linux"
+pathSharedfoldDock=$4
+pathSharedfoldHost=$4
+fi
+
+if [ $# -eq 5 ]
+then
+echo "WORKS ONLY IN DOCKER CONTAINER!!!!!!!!!!!!!!!!!!!!"
+pathSharedfoldDock=$4
+pathSharedfoldHost=$(cat $5)
+if ! test -f "$5"; then
+    echo "ConfigurationFile does not exists. You dont have access to the dockerFileGenerator power. Run again the script for DockerFileGeneratorGUI to recreate the file."
+    echo "Docker container failed!! check log"
+    exit 1
+fi
+echo $pathSharedfoldHost
+fi
+
+dockerTempName=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+dockerName=$(echo "$3" | tr '[:upper:]' '[:lower:]')
+
+
+j="$( basename "$1" )"
+echo ${j}
+pathSharedfoldDocks=$pathSharedfoldDock/${j}_wExternalFile
 if [ -d "$pathSharedfoldDocks" ]; then
     echo "Error: ${pathSharedfoldDocks} already exists. Use another name or delete the folder!!!!"
   exit 1
@@ -22,41 +56,45 @@ else
   echo "Installing files in ${pathSharedfoldDocks}..."
 
 fi
-mkdir $2/${j}_wExternalFile
+
+mkdir -p $pathSharedfoldDock/${j}_wExternalFile
 
 
-retry cp -r $1/* $2/${j}_wExternalFile
+retry cp -r $1/* $pathSharedfoldDock/${j}_wExternalFile
 sync
 swapoff -a
 swapon -a
 printf '\n%s\n' 'Ram-cache and Swap Cleared'
 echo 3 > /proc/sys/vm/drop_caches
 
-retry cp downloadSoft.sh $2/${j}_wExternalFile
-
+retry cp -r ./configurationFile.txt $pathSharedfoldDock/${j}_wExternalFile/
+sync
+swapoff -a
+swapon -a
+printf '\n%s\n' 'Ram-cache and Swap Cleared'
+echo 3 > /proc/sys/vm/drop_caches
+retry cp -r ./downloadSoft.sh $pathSharedfoldDock/${j}_wExternalFile/
 sync
 swapoff -a
 swapon -a
 printf '\n%s\n' 'Ram-cache and Swap Cleared'
 echo 3 > /proc/sys/vm/drop_caches
 
-retry cp ./softwareList.txt $2/${j}_wExternalFile
 
-sync
-swapoff -a
-swapon -a
-printf '\n%s\n' 'Ram-cache and Swap Cleared'
-echo 3 > /proc/sys/vm/drop_caches
-
-if ! docker build --platform linux/amd64 $2/${j}_wExternalFile/ -t youwillneverusethiname; then
-    echo "Docker container failed!! check log"
+if ! docker build --platform linux/amd64 $pathSharedfoldDock/${j}_wExternalFile -t $dockerTempName; then
     exit 1
 fi
 
-if ! docker run -tv $2/${j}_wExternalFile:/scratch youwillneverusethiname bash -c "cat /scratch/softwareList.txt | /scratch/downloadSoft.sh"; then
+echo "Step library Install. Might take some time. "
+if ! docker run --platform linux/amd64 -tv $pathSharedfoldHost/${j}_wExternalFile/:/scratch $dockerTempName bash -c "cat /scratch/configurationFile.txt | /scratch/downloadSoft.sh"; then
 echo "Wewweee uagliungell acca c'è qualcosa che non va sfaccimm!!!"
 exit 1
 fi
-cat tail.txt >> $2/${j}_wExternalFile/Dockerfile
-
+cat ./tail.txt >> $pathSharedfoldDock/${j}_wExternalFile/Dockerfile
+sync
+swapoff -a
+swapon -a
+printf '\n%s\n' 'Ram-cache and Swap Cleared'
+echo 3 > /proc/sys/vm/drop_caches
 echo 'DockerFile generation is done. Locate in DockerFolder and build your final docker.\n'
+
